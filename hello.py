3c298971369094782d9525bf9b8401e7229d6c3f
Thapani Sawaengsri <45223662+tsaw8@users.noreply.github.com>
2025-05-19 13:31:26 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("Hello Kartik!")
+print("Hello Kartik! In silent halls where logic flows,
+The coder’s craft in silence grows.
+A line, a loop, a nested call,
+Each function built to never fall.")
